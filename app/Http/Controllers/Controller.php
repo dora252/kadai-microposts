70a3d770d7095a95bfacde82979bf0bce48a9edd
dora252 <nikofudora184@gmail.com>
2019-05-18 08:06:47 +0000

    post

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,12 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    
+    public function counts($user){
+        $count_micropsts = $user->microposts()->count();
+        
+        return [
+            'count_microposts' => $count_micropsts,
+            ];
+    }
 }
